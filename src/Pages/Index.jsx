5ffdc841f7aaffa6b08f117052205c1209bae039
Matt Kars <mattkars@gmail.com>
2024-07-15 19:10:58 -0400

    initial parking page

---
+++ src/Pages/Index.jsx
@@ -0,0 +1,16 @@
+const Index = () => {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold text-blue-500">Karsyz Robotics</h1>
+      <p className="text-slate-100">Check back later</p>
+      <a
+        href="mailto:matt@karsyzrobotics.com"
+        className="underline font-semibold text-blue-400"
+      >
+        email
+      </a>
+    </div>
+  );
+};
+
+export default Index;
