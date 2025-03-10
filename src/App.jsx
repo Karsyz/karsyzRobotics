40e7e31d4bdf7a4039d3cf3df975443f516968ed
Matt Kars <mattkars@gmail.com>
2025-03-10 16:39:47 -0400

    fix: added route redirects

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,24 @@ import {
   createBrowserRouter,
   Route,
   createRoutesFromElements,
-  RouterProvider
-} from 'react-router-dom'
+  RouterProvider,
+} from 'react-router-dom';
 
 // Pages
-import Index from './Pages/Index'
-import Whiteboard from './Pages/Whiteboard'
-import Portfolio from './Pages/Portfolio'
-
+import Index from './Pages/Index';
+import Whiteboard from './Pages/Whiteboard';
+import Portfolio from './Pages/Portfolio';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path='/' element={<Index />} />
+      <Route path="/" element={<Index />} />
       <Route path="/whiteboard" element={<Whiteboard />} />
       <Route path="/portfolio" element={<Portfolio />} />
     </>
   )
-)
-
+);
 
 export default function App() {
-  return (
-    <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />;
 }
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "_redirects",
+          dest: ".",
+        },
+      ],
+
+
+
+
+
+      
+    }),
+  ],
   define: {
-    "process.env": {} // Polyfill process to avoid the error
-  }
-})
+    "process.env": {}, // Polyfill process to avoid the error
+  },
+});
