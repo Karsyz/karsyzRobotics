81343d033f28722b7650b817c28522e64d78f875
Matt Kars <mattkars@gmail.com>
2025-04-07 14:47:20 -0400

    added freebies
    added affiliate message and links to freebies

--- src/Data/freebies.js
+++ src/Data/freebies.js
@@ -5,9 +5,10 @@ export const freebies = [
     headline: 'Replacement Plunger for Pampered Chef Mixer and Dispenser',
     description:
       'This is a 3D printed replacement plunger designed for the Pampered Chef Mixer and Dispenser. The original plunger is known to fail after just a couple of years of use, and this model aims to provide a durable, cost-effective solution.',
-    imgSrc: '/images/freebies/f1.PNG',
+    imgSrc: '/images/freebies/resized/f1.PNG',
     imgAlt: 'Pampered Chef Replacement Plunger',
-    href: '/freebies/pamperedChefPlunger.stl',
+    href: '/freebies/pampered_chef_plunger.stl',
+    affHref: 'https://amzn.to/3XO5cef',
   },
   {
     id: 2,
@@ -15,9 +16,46 @@ export const freebies = [
     headline: 'A tool for making tarts easier',
     description:
       'The Tart Tool is a 3D-printed kitchen essential that makes tart-making simple. Its circular shape presses pastry evenly in standard tins, with an ergonomic handle for ease of use. Designed to be easy to print, easy to clean, and durable, it ensures perfect tart shells every time!',
-    imgSrc: '/images/freebies/f2.PNG',
+    imgSrc: '/images/freebies/resized/f2.PNG',
     imgAlt: 'Tart tool',
-    href: '/freebies/tartTool.stl',
+    href: '/freebies/tart_tool.stl',
+    affHref: 'https://amzn.to/3R7cYfl',
+  },
+  {
+    id: 3,
+    name: 'Keurig Kcup Reusable Filter Holder',
+    headline:
+      'Spill-Free Keurig K-Cup Reusable Filter Holder – Brew Smart, Clean Easy',
+    description:
+      'Say goodbye to messy coffee grounds with this innovative Keurig K-Cup Reusable Filter Holder. Designed to keep your counter spotless while filling, it also makes pouring leftover grounds back into the bag a breeze. Enjoy a cleaner, hassle-free brewing experience every time!',
+    imgSrc: '/images/freebies/resized/f3.PNG',
+    imgAlt: 'Keurig Kcup Reusable Filter Holder',
+    href: '/freebies/keurig_kcup_filler_tray.stl',
+    affHref: 'https://amzn.to/4iZSeCs',
+  },
+  {
+    id: 4,
+    name: 'DeLonghi Dedica Grinder Kcup Holder Attachment',
+    headline:
+      'De’Longhi Dedica Grinder K-Cup Holder Attachment – Seamless Grinding for Reusable Keurig K-Cups',
+    description:
+      'Elevate your coffee routine with the De’Longhi Dedica Grinder K-Cup Holder Attachment. Designed to securely hold a reusable Keurig K-Cup directly on your grinder, this attachment lets you grind fresh coffee beans straight into the filter with precision and ease. Perfect for a mess-free, customized brew every time!',
+    imgSrc: '/images/freebies/resized/f4.PNG',
+    imgAlt: 'DeLonghi Dedica Grinder Kcup Holder Attachment',
+    href: '/freebies/delonghi_grinder_kcup_attachment.stl',
+    affHref: 'https://amzn.to/4jl96n5',
+  },
+  {
+    id: 5,
+    name: 'DeLonghi Dedica Grinder Guard',
+    headline:
+      'De’Longhi Dedica Grinder Guard – Keep Your Counter Clean While Grinding',
+    description:
+      'Tired of coffee grinds scattering everywhere? The De’Longhi Dedica Grinder Guard is here to help. This clever accessory fits your grinder to contain stray grinds when filling your portafilter, ensuring a tidy counter and a smoother brewing process. Grind with confidence and zero mess!',
+    imgSrc: '/images/freebies/resized/f5.PNG',
+    imgAlt: 'DeLonghi Dedica Grinder Kcup Holder Attachment',
+    href: '/freebies/delonghi_grinder_guard.stl',
+    affHref: 'https://amzn.to/4jl96n5',
   },
   // {
   //   id: 3,
--- src/Pages/Freebies.jsx
+++ src/Pages/Freebies.jsx
@@ -12,15 +12,28 @@ function Freebies() {
       <Navbar />
       <Breadcrumbs />
       <div className="pt-16">
-        <section className="py-16 px-6">
-          <h2 className="text-3xl font-bold text-gray-800 mb-12 text-center w-full">
-            Free Models
-          </h2>
+        <section className="py-16 px-6 ">
+          <div className='flex flex-col items-center mb-12'>
+            <h2 className="text-3xl font-bold text-gray-800 text-center w-full">
+              Free Models
+            </h2>
+            <p>
+              Clicking the cards below will download the stl file as well as
+              open an affiliate link to amazon in a new tab. Thanks for your support.
+            </p>
+          </div>
           <div className="container mx-auto">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 justify-items-center gap-y-10">
               {freebies.map((model) => {
                 return (
-                  <a href={model.href} download={`/freebies/${model.href}`} key={model.id}>
+                  <a
+                    href={model.href}
+                    download={`/freebies/${model.href}`}
+                    key={model.id}
+                    onClick={(e) => {
+                      window.open(model.affHref, '_blank', 'noopener,noreferrer');
+                    }}
+                  >
                     <div
                       id={`product-card-${model.id}`}
                       className="relative w-full sm:w-[300px] border border-gray-200 rounded-lg overflow-hidden shadow-md hover:-translate-y-1 transition-transform duration-200 cursor-pointer group flex flex-col h-full"
